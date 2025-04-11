56382b687d27a77d0725b5d910a90be2e1dd1637
okoyfoeciov <92897177+okoyfoeciov@users.noreply.github.com>
2025-04-12 03:08:50 +0700

    Create dev.nix

---
+++ .idx/dev.nix
@@ -0,0 +1,28 @@
+# To learn more about how to use Nix to configure your environment
+# see: https://firebase.google.com/docs/studio/customize-workspace
+{ pkgs, ... }: {
+  # Which nixpkgs channel to use.
+  channel = "stable-24.05"; # or "unstable"
+  # Use https://search.nixos.org/packages to find packages
+  packages = [
+    pkgs.cargo
+    pkgs.rustc
+    pkgs.rustfmt
+  ];
+  # Sets environment variables in the workspace
+  env = {
+    RUST_SRC_PATH = "${pkgs.rustPlatform.rustLibSrc}";
+  };
+  idx = {
+    # Search for the extensions you want on https://open-vsx.org/ and use "publisher.id"
+    extensions = [];
+    workspace = {
+      onCreate = {
+        # Open editors for the following files by default, if they exist:
+        default.openFiles = ["src/main.rs"];
+      };
+    };
+    # Enable previews and customize configuration
+    previews = {};
+  };
+}
