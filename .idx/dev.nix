7b334601886418235325b13651ad3d45eb92f0f2
kyle <92897177+okoyfoeciov@users.noreply.github.com>
2025-04-12 03:12:40 +0000

    update

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -20,9 +20,6 @@
     # Search for the extensions you want on https://open-vsx.org/ and use "publisher.id"
     extensions = [
       "rust-lang.rust-analyzer"
-      "tamasfe.even-better-toml"
-      "serayuzgur.crates"
-      "vadimcn.vscode-lldb"
     ];
     workspace = {
       onCreate = {
